d205afa6972bbb8d614d05e1ceab0901a4b6c464
bekoszn <berkeozguder@gmail.com>
2024-08-04 21:51:06 +0300

    Initial Commit

---
+++ AburCuburApp/AburCuburAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AburCuburAppApp.swift
+//  AburCuburApp
+//
+//  Created by Berke Özgüder on 4.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct AburCuburAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
